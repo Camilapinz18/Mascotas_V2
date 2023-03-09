67cd5c3f463876ec68f51015a95dcad732e474b2
Camilo Burbano <camiloburbano82@gmail.com>
2023-03-09 13:50:01 -0500

    oneUser

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -45,3 +45,15 @@ exports.deleteUser = async (req, res) => {
         res.status(500).json({ msg: 'Error al eliminar el usuario' });
     }
 }
+
+exports.oneUser = async (req, res) => {
+    try {
+        let user = await User.findOne({ idCard: req.params.id });
+        if (!user) {
+            res.status(404).json({ msg: 'no existe el usuario' });
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ msg: 'Error al buscar el usuario' });
+    }
+}
--- src/routes/user.js
+++ src/routes/user.js
@@ -6,5 +6,6 @@ router.get('/all', userController.allUsers);
 router.post('/register', userController.createUser);
 router.put('/update/:id', userController.updateUser);
 router.delete('/delete/:id', userController.deleteUser);
+router.get('/:id', userController.oneUser);
 
 module.exports = router;
